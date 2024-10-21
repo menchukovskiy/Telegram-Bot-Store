e6973dbf86c34fde5abdffc3327979add1501e01
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-10-21 18:14:32 +0300

    bot v 0.0.4

--- bot/src/App.js
+++ bot/src/App.js
@@ -1,9 +1,16 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useTelegram} from "./hooks/useTelegram";
 import './App.css';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
       tg.ready();
@@ -11,7 +18,19 @@ function App() {
 
   return (
     <div className="App">
-      BOT
+      <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
     </div>
   );
 }
