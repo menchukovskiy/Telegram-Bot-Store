84adf137ac1591a2d0f734431ba1491d738453ae
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-10-21 18:05:23 +0300

    BOT V 0.0.1

--- server/controllers/TelegramController.js
+++ server/controllers/TelegramController.js
@@ -1,12 +1,31 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '7729915916:AAG8OXiGPL5nHpIuE-_I8yCs-rHi05Yk5pE';
 
+const token = '7729915916:AAG8OXiGPL5nHpIuE-_I8yCs-rHi05Yk5pE';
+const webAppUrl = 'https://golden-paletas-f053f6.netlify.app/';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+
+
+
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Received your message111131231');
+    const text = msg.text;
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    } else {
+        bot.sendMessage(chatId, 'Received your message');
+    }
+    
+    
   });
+
+
 
 
  class TelegramStore {
