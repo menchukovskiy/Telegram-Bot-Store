ce85e0e67991071701ef302026d90aae38e4b241
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-10-21 18:17:12 +0300

    bot change error

--- bot/src/App.js
+++ bot/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {useTelegram} from "./hooks/useTelegram";
+
 import './App.css';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -9,12 +9,10 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
+  
   const [value, setValue] = useState(0);
 
-  useEffect(() => {
-      tg.ready();
-  }, [])
+  
 
   return (
     <div className="App">
