1ea077cdf259f69f795f8ce9c4173a36c0943d42
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-10-21 18:10:28 +0300

    bot v 0.0.3

--- bot/src/App.js
+++ bot/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import {useTelegram} from "./hooks/useTelegram";
 import './App.css';
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
+  useEffect(() => {
+      tg.ready();
+  }, [])
+
   return (
     <div className="App">
-      
+      BOT
     </div>
   );
 }
