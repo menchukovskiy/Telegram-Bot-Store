9ac51b588f1f44c2dda62503d581204143bb3cb9
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-10-21 19:11:41 +0300

    test size

--- bot/src/App.js
+++ bot/src/App.js
@@ -1,17 +1,18 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 import './App.css';
 import BottomNavBar from './components/BottomNavBar';
 
 
 function App() {
-  
-  
+
+
 
   const tg = window.Telegram.WebApp;
 
   console.log(tg)
   useEffect(() => {
+    tg.expand()
     tg.ready();
   }, [tg])
 
