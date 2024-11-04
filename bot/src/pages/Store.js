011d8a0c85809168ab6506ffdb921212faf0bf5b
Vitalii Menchukovskyi <menchukovskiy@gmail.com>
2024-11-04 17:00:42 +0200

    Bot Stoe 2.1

--- bot/src/pages/Store.js
+++ bot/src/pages/Store.js
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const Store = () => {
 
 
-    const dispatch = useDispatch()
+    
     const dataStore = useSelector(state => state.store)
 
     console.log(dataStore)
